626099092e7c5a2c9f43b244692ea10ecec85591
Evert Vanderstadt <evert.vanderstadt.w9991@student.hogent.be>
2018-09-12 21:19:59 +0200

    pls

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+const api_url = 'https://spotify-listen-along-backend.herokuapp.com/';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quotes: [],
+      url: ""
+    };
+  }
+  componentWillMount() {
+    fetch(api_url+'login')
+      .then(response => {
+        if (response.ok) {
+          response.text().then((text)=>{ this.setState({url:text}, () => console.log("Got the URL!", text)) });
+          /* return this.setState({url:response.text().then((text)=>{ return text })}); */
+        } else {
+          console.log("Couldn't get login URL", response);
+        }
+      });
+  }
+
+  render() {
+    return (
+      <Button color="primary" size="lg" block href={this.state.url}>
+        Log in
+      </Button>
+    );
+  }
+}
+
+export default Login;
